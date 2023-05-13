3913420a31e51936739bab7b129b9bcdcc4d6162
khaizr0 <caophankhai123@gmail.com>
2023-05-14 06:08:10 +0700

    Email OTP

--- QuanLyChiTieu (2)/QuanLyChiTieu/fQuenMatKhau.cs
+++ QuanLyChiTieu (2)/QuanLyChiTieu/fQuenMatKhau.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,15 +14,67 @@ namespace QuanLyChiTieu
 {
     public partial class fQuenMatKhau : Form
     {
+        string randomcode;
+        public static string to;
         public fQuenMatKhau()
         {
             InitializeComponent();
         }
 
+        private void btnlayma_Click(object sender, EventArgs e)
+        {
+
+            // send code
+            string from, pass, messagebody;
+            Random rand = new Random();
+            randomcode = (rand.Next(999999)).ToString();
+            MailMessage message = new MailMessage();
+            to = (textBox1.Text).ToString();
+            from = "khai.sendmail@gmail.com";
+            pass = "xsjayaiphjedablr";
+            messagebody = $"Mã của bạn là: {randomcode}";
+            message.To.Add(to);
+            message.From = new MailAddress(from);
+            message.Body = messagebody;
+            message.Subject = "Mã lấy lại mật khẩu";
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com");
+            smtp.EnableSsl = true;
+            smtp.Port = 587;
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtp.Credentials = new NetworkCredential(from, pass);
+            try
+            {
+                smtp.Send(message);
+                MessageBox.Show("Gửi mã thành công, vui lòng check hòm thư của bạn");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
-            fResetPass fResetPass = new fResetPass();
-            fResetPass.Show();
+            if (randomcode == (textBox2.Text).ToString())
+            {
+                to = textBox1.Text;
+                fResetPass fResetPass = new fResetPass();
+                fResetPass.Show();
+            }
+            else
+            {
+                MessageBox.Show("Sai mã, vui lòng nhập lại.");
+            }
+        }
+
+        private void fQuenMatKhau_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- QuanLyChiTieu (2)/QuanLyChiTieu/fResetPass.cs
+++ QuanLyChiTieu (2)/QuanLyChiTieu/fResetPass.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Security.Cryptography;
 
 namespace QuanLyChiTieu
 {
     public partial class fResetPass : Form
     {
+        string emaildt = fQuenMatKhau.to;
         public fResetPass()
         {
             InitializeComponent();
@@ -20,6 +23,38 @@ namespace QuanLyChiTieu
         private void newpassword_Load(object sender, EventArgs e)
         {
 
+        }
+        Modify modify = new Modify();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string password = textBox2.Text;
+            if (textBox1.Text == textBox2.Text)
+            {
+                string matkhau = textBox1.Text;
+                string email = emaildt;
+                string passHash = GenerateHash(matkhau);
+
+                string query = "update [regtable] set [Password]='" + passHash + "' where Email='" + email + "'";
+                modify.Command(query);
+                MessageBox.Show("Bạn đã đổi mật khẩu thành thông, vui lòng chuyển sang trang đăng nhập.", "Thông báo!");
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Mật Khẩu bạn nhập không khớp, vui lòng nhập lại.");
+            }
+        }
+        private static string GenerateHash(string toHash)
+        {
+            var crypt = new SHA256Managed();
+            string hash = String.Empty;
+            byte[] crypto = crypt.ComputeHash(Encoding.ASCII.GetBytes(toHash));
+            foreach (byte theByte in crypto)
+            {
+                hash += theByte.ToString("x2");
+            }
+            return hash;
         }
     }
 }
